ed4e78eaf0de017a63cafad3d6c7e66cf3443b34
Samuel <Samuelpmontague@gmail.com>
2021-08-03 16:52:07 -0500

    fixed model files

--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,42 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
+
 //Render last workout
 router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+    
 })
+
 //Update by ID
 router.put("/api/workouts/:id", ({ body }, res) => {
-    db.Workout.create(body) 
+    Workout.create({}) 
     .then(({ _id }) => db.Workout.findOneAndUpdate({}, { $push: { exercise: _id } }, { new: true }))
     .then(dbWorkout => {
         res.json(dbWorkout);
     })
     .catch(err => {
-        res.json(err);
+        res.status(400).json(err);
     });
 });
-//Example exercise 15 -- exercise 14 according to Gary ** lines 31 and down
-// app.post("/submit", ({ body }, res) => {
-//     db.Note.create(body)
-//       .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
-//       .then(dbUser => {
-//         res.json(dbUser);
-//       })
-//       .catch(err => {
-//         res.json(err);
-//       });
-//   });
+
+// Get the last 7 workouts
+router.get("api/workouts/range", ({body},  res) =>{
+    Workout.find().limit(7)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+})
+
+// Create a workout
 router.post("/api/workouts", ({ body }, res) => {
     Workout.create(body)
         .then(dbWorkout => {
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const router = require("./routes/api.js");
 
 const PORT = process.env.PORT || 3000;
 
-const User = require("./userModel.js");
 const app = express();
 
 app.use(logger("dev"));
@@ -14,11 +14,22 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+let db = mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workouttrackerDB",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  }
+);
 
+// routes
 app.use(router);
 require("./routes/html.js")(app);
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
+  console.log(`App running on port ${PORT}!`);
+});
+
+module.exports = db;
